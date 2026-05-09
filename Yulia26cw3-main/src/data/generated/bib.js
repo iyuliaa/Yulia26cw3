b211f5e792877e7ce2dbbb5c8684e765eedd7c30
Indah Yulia <akadiralwi@gmail.com>
2026-05-09 14:48:32 +0100

    Regenerate SurVis data

---
+++ Yulia26cw3-main/src/data/generated/bib.js
@@ -0,0 +1,119 @@
+﻿const generatedBibEntries = {
+    "alam2025sentiment": {
+        "author": "Alam, Md Shah and Mrida, Md Sabbir Hossain and Rahman, Md Atikur",
+        "doi": "10.63125/r3sq6p80",
+        "journal": "American Journal of Scholarly Research and Innovation",
+        "keywords": "C3 Survey and Conceptual Background, sentiment analysis, social media, NLP, AI, public opinion mining",
+        "number": "1",
+        "pages": "63--100",
+        "title": "Sentiment Analysis in Social Media: How Data Science Impacts Public Opinion Knowledge Integrates Natural Language Processing (NLP) with Artificial Intelligence (AI)",
+        "type": "article",
+        "volume": "4",
+        "year": "2025"
+    },
+    "dalkilic2026smart": {
+        "author": "Dalkilic, Feristah and Karaarslan, Enis",
+        "doi": "10.61969/jai.1883089",
+        "journal": "Journal of AI",
+        "keywords": "C2 Social Media and Public Policy Sentiment Analysis, smart city, social media, citizen feedback, NLP, Twitter, X",
+        "pages": "70--84",
+        "title": "Smart City Insights from Social Media: A System for Analyzing Citizen Feedback in the Aegean Region",
+        "type": "article",
+        "volume": "10",
+        "year": "2026"
+    },
+    "fu2024deciphering": {
+        "author": "Fu, Xinyu and Sanchez, Thomas W. and Li, Chaosu and Junqueira, Juliana Reu",
+        "doi": "10.1080/01944363.2024.2309259",
+        "journal": "Journal of the American Planning Association",
+        "keywords": "C2 Social Media and Public Policy Sentiment Analysis, ChatGPT, citizen feedback, urban planning, LDA, sentiment analysis",
+        "number": "4",
+        "pages": "728--741",
+        "title": "Deciphering Public Voices in the Digital Era: Benchmarking ChatGPT for Analyzing Citizen Feedback in Hamilton, New Zealand",
+        "type": "article",
+        "volume": "90",
+        "year": "2024"
+    },
+    "greco2024nlpguard": {
+        "author": "Greco, Salvatore and Zhou, Ke and Capra, Licia and Cerquitelli, Tania and Quercia, Daniele",
+        "doi": "10.1145/3686924",
+        "journal": "Proceedings of the ACM on Human-Computer Interaction",
+        "keywords": "C4 Ethical AI and Fairness in NLP, fairness, protected attributes, bias, NLP classifiers, responsible AI",
+        "number": "CSCW2",
+        "pages": "1--25",
+        "title": "NLPGuard: A Framework for Mitigating the Use of Protected Attributes by NLP Classifiers",
+        "type": "article",
+        "volume": "8",
+        "year": "2024"
+    },
+    "hamid2025sentiment": {
+        "author": "Hamid, Ricky Bahari and Andriansyah, Chandra and Sensuse, Dana Indra and Lusa, Sofian and Elisabeth, Damayanti and Safitri, Nadya",
+        "doi": "10.52436/1.jutif.2025.6.5.4991",
+        "journal": "Jurnal Teknik Informatika (JUTIF)",
+        "keywords": "C1 E-government Mobile Application Review Mining, e-government, RoBERTa, BERTopic, topic modelling, app reviews",
+        "number": "5",
+        "pages": "3188--3203",
+        "title": "Sentiment Analysis and Topic Modeling for Discovering Knowledge in Indonesian Mobile Government Applications",
+        "type": "article",
+        "volume": "6",
+        "year": "2025"
+    },
+    "kurniawati2026analysis": {
+        "author": "Kurniawati, Yulia and Hamid, Ricky Bahari and Sensuse, Dana Indra and Lusa, Sofian and Putro, Prasetyo Adi Wibowo and Indriasari, Sofiyanti",
+        "doi": "10.52436/1.jutif.2026.7.2.5415",
+        "journal": "Jurnal Teknik Informatika (JUTIF)",
+        "keywords": "C2 Social Media and Public Policy Sentiment Analysis, public policy, X, Twitter, SVM, IndoBERT, PDP Law",
+        "number": "2",
+        "pages": "1007--1027",
+        "title": "Analysis of Public Sentiment Indonesia's Personal Data Protection Law: A Comparison of SVM and IndoBERT on X Platform",
+        "type": "article",
+        "volume": "7",
+        "year": "2026"
+    },
+    "marutho2025benchmarking": {
+        "author": "Marutho, Dhendra and Utomo, Victor Gayuh",
+        "doi": "10.26623/transformatika.v23i1.12095",
+        "journal": "Jurnal TRANSFORMATIKA",
+        "keywords": "C1 E-government Mobile Application Review Mining, IndoBERT, transformer models, e-government, sentiment classification, benchmarking",
+        "number": "1",
+        "pages": "85--95",
+        "title": "Benchmarking IndoBERT and Transformer Models for Sentiment Classification on Indonesian E-Government Service Reviews",
+        "type": "article",
+        "volume": "23",
+        "year": "2025"
+    },
+    "sarvestani2025nlp": {
+        "author": "Sarvestani, Milad",
+        "doi": "10.1109/ACCESS.2025.3641669",
+        "journal": "IEEE Access",
+        "keywords": "C1 E-government Mobile Application Review Mining, NLP, citizen feedback, government apps, sentiment analysis, topic modelling",
+        "pages": "210360--210377",
+        "title": "An NLP-Based Framework for Sentiment and Topic Analysis of Citizen Feedback on U.K. Government Mobile Applications",
+        "type": "article",
+        "volume": "13",
+        "year": "2025"
+    },
+    "savitri2025implementation": {
+        "author": "Savitri, Ratna and Rizki, Fido and Sobri, Ahmad",
+        "doi": "10.18860/mat.v17i2.32059",
+        "journal": "MATICS: Jurnal Ilmu Komputer dan Teknologi Informasi",
+        "keywords": "C1 E-government Mobile Application Review Mining, SIGNAL, IndoBERT, BERT, sentiment analysis, user reviews",
+        "number": "2",
+        "pages": "67--75",
+        "title": "Implementation of BERT in Sentiment Analysis of National Digital Samsat (SIGNAL) User Reviews Based on Machine Learning",
+        "type": "article",
+        "volume": "17",
+        "year": "2025"
+    },
+    "tyagi2023demystifying": {
+        "author": "Tyagi, Nemika and Bhushan, Bharat",
+        "doi": "10.1007/s11277-023-10312-8",
+        "journal": "Wireless Personal Communications",
+        "keywords": "C3 Survey and Conceptual Background, NLP, smart city, IoT, machine learning, smart governance",
+        "pages": "857--908",
+        "title": "Demystifying the Role of Natural Language Processing (NLP) in Smart City Applications: Background, Motivation, Recent Advances, and Future Research Directions",
+        "type": "article",
+        "volume": "130",
+        "year": "2023"
+    }
+};
